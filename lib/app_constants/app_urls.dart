88e828d94b1aad322dafa9a9087d9d1bdeb8b88f
Mikhil Mohan C <mikhilmohan13@gmail.com>
2026-09-21 12:54:01 +0530

    fix: update base URL for the application

--- lib/app_constants/app_urls.dart
+++ lib/app_constants/app_urls.dart
@@ -1,5 +1,5 @@
 class AppUrls {
-  static const String baseUrl = "https://vqp6fbbv-8001.inc1.devtunnels.ms";
+  static const String baseUrl = "https://417sptdw-8006.inc1.devtunnels.ms";
   static const String hostUrl = "$baseUrl/host";
 
   static const String registerUrl = "$hostUrl/hosts/";
